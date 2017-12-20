1b72bb25b0d0cae67dc0f658d98b2c275702f2f8
Jean Escalante <jean.r.escalante@gmail.com>
2017-12-19 19:44:30 -0800

    Implemented Difficulties
    Added difficulty options and prompt to get the game started.

---
+++ 2_Terminal_Hacker/2_Terminal_Hacker/Assets/Hacker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+        ShowMainMenu("Hello, Sombra");
+    }
+
+    void ShowMainMenu (string greeting) {
+        Terminal.ClearScreen();
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("What would you like to hack today?");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Press 1 for Snow White's Diary");
+        Terminal.WriteLine("Press 2 for Gringotts Bank");
+        Terminal.WriteLine("Press 3 for Athena");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Enter your selection: ");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
