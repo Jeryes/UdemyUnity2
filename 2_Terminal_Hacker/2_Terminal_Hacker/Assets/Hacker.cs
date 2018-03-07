59b6effab874b47bfdca976fe38cceea0f8208a8
Jean Escalante <jean.r.escalante@gmail.com>
2018-03-06 22:42:48 -0800

    Enums, Game States, and gitignore
    Added game states to the code as well as an enumerator to handle them.
    Minor edit to gitignore.

--- 2_Terminal_Hacker/2_Terminal_Hacker/Assets/Hacker.cs
+++ 2_Terminal_Hacker/2_Terminal_Hacker/Assets/Hacker.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    // Game State
+    int level;
+
+    enum Screen { MainMenu, Password, Win }
+    Screen currentScreen;
+
+    // Use this for initialization
+    void Start () {
         ShowMainMenu();
     }
 
     void ShowMainMenu () {
+        currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
         Terminal.WriteLine("What would you like to hack today?");
         Terminal.WriteLine("");
@@ -21,14 +28,34 @@ public class Hacker : MonoBehaviour {
     }
 
     void OnUserInput(string input)
-    {            
+    {
         if (input == "menu") {
             ShowMainMenu();
         }
-        else if (input == "69") {
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+    }
+
+    void RunMainMenu(string input)
+    {
+        if (input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else if (input == "69")
+        {
             Terminal.WriteLine("You nasty boy!");
         }
-        else if (input == "mario") {
+        else if (input == "mario")
+        {
             Terminal.WriteLine("▒▒▒▒▒▒▒▒▒▄▄▄▄▒▒▒▒▒▒▒");
             Terminal.WriteLine("▒▒▒▒▒▒▄▀▀▓▓▓▓█▒▒▒▒▒▒");
             Terminal.WriteLine("▒▒▒▒▄▀▓▓▄▄██████▄▒▒▒");
@@ -44,8 +71,16 @@ public class Hacker : MonoBehaviour {
             Terminal.WriteLine("▒▒▒▒▒█▀▀▀▀▀█▀▀▒▒▒▒▒▒");
             Terminal.WriteLine("▒▒▒▒▒█▄▄▄▄▄▄█▒▒▒▒▒▒▒");
         }
-        else {
+        else
+        {
             Terminal.WriteLine("Please enter a valid selection.");
         }
+    }
+
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("You have chosen level " + level);
+        Terminal.WriteLine("Please enter your password.");
     }
 }
